7cd2244aa0c6e4bbe350287e7968da7ba6fe34ba
Warren Yao <warrenyca905@gmail.com>
2019-09-14 20:28:24 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -26,5 +26,18 @@ class TextAnalyzer
     arr2 = {}
  
     arr1.map do |c|
-    arr2[c] =  arr.count(c)
+      arr2[c] =  arr.count(c)
+    end
+  
+    biggest = { arr2.keys.first => arr2.values.first }
+ 
+    arr2.each do |key, value|
+      if value > biggest.values.first
+        biggest = {}
+        biggest[key] = value
+      end
+    end
+ 
+    biggest
   end
+end
