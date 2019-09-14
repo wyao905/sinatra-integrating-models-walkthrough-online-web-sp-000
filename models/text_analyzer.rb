e1088d79ad986f9a34c9138948fbf7d7771321c8
Warren Yao <warrenyca905@gmail.com>
2019-09-14 20:23:48 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -19,4 +19,12 @@ class TextAnalyzer
     text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
   
-  
+  def most_used_letter
+    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
+    arr = s1.split('')
+    arr1 = arr.uniq
+    arr2 = {}
+ 
+    arr1.map do |c|
+    arr2[c] =  arr.count(c)
+  end
