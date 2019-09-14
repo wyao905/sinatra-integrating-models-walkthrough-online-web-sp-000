1cbbbec848df4a1efe04611dcb0ff7f6ac8e6da4
Warren Yao <warrenyca905@gmail.com>
2019-09-14 20:23:14 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,19 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end
+  
+  def count_of_words
+    words = text.split(" ")
+    words.count
+  end
+  
+  def count_of_vowels
+    text.scan(/[aeiou]/).count
+  end
+  
+  def count_of_consonants
+    text
